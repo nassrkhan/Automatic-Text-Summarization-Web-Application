6b13cd46890c6f46a34ba4b92a0121735dd3f7a2
Nasrullah Khan <107609319+nassrkhan@users.noreply.github.com>
2023-11-28 16:40:10 +0500

    1st Day task (28/11)

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
+# For Flash Messages
+from django.contrib import messages
 
 # Create your views here.
 def main(request):
-    return HttpResponse("Hello, World!")
+    messages.success(request, 'The post has been created successfully.')
+    #return HttpResponse("Hello, World!")
+    return render(request,'message.html')
